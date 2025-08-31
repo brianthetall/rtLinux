b1c6770b423b157019593739f343242b261da54e
Brian Wagner <brianthetall@gmail.com>
2025-08-31 04:02:16 +0100

    Websockets function!

--- websockets/client.c
+++ websockets/client.c
@@ -8,62 +8,70 @@ static int callback_client(struct lws *wsi,
                            void *user, 
                            void *in, 
                            size_t len) {
-    switch (reason) {
-        case LWS_CALLBACK_CLIENT_ESTABLISHED:
-            // Send a message once the connection is established
-            lws_write(wsi, (unsigned char *)"Hello, Server!", 14, LWS_WRITE_TEXT);
-            break;
-        case LWS_CALLBACK_CLIENT_RECEIVE:
-            // Print the received message
-            printf("Received: %s\n", (char *)in);
-            break;
-        default:
-            break;
-    }
-    return 0;
+  printf("Entered callback function\n");
+  switch (reason) {
+  case LWS_CALLBACK_CLIENT_ESTABLISHED:
+    // Send a message once the connection is established
+    printf("SENDING....\n");
+    /*
+      LWS_WRITE_TEXT, LWS_WRITE_BINARY, LWS_WRITE_CONTINUATION, LWS_WRITE_PING, LWS_WRITE_PONG
+      the send buffer has to have LWS_PRE bytes valid BEFORE the buffer pointer you pass to lws_write().
+    */
+    unsigned char buf[LWS_PRE + 128];
+    strcpy(&buf[LWS_PRE], "Hello, Server...");
+    lws_write(wsi, &buf[LWS_PRE], strlen("Hello, Server..."), LWS_WRITE_TEXT);
+    break;
+  case LWS_CALLBACK_CLIENT_RECEIVE:
+    // Print the received message
+    printf("Received: %s\n", (char *)in);
+    break;
+  default:
+    break;
+  }
+  return 0;
 }
 
 static struct lws_protocols protocols[] = {
-    {
-        "client-protocol",
-        callback_client,
-        0,
-        128,
-    },
-    { NULL, NULL, 0, 0 } // terminator
+  {
+    "client-protocol",
+    callback_client,
+    0,
+    128,
+  },
+  { NULL, NULL, 0, 0 } // terminator
 };
 
 int main() {
-    struct lws_context_creation_info info;
-    struct lws_context *context;
+  struct lws_context_creation_info info;
+  struct lws_context *context;
 
-    memset(&info, 0, sizeof(info));
-    info.port = CONTEXT_PORT_NO_LISTEN; // No listening port for client
-    info.protocols = protocols;
+  memset(&info, 0, sizeof(info));
+  info.port = CONTEXT_PORT_NO_LISTEN; // No listening port for client
+  info.protocols = protocols;
 
-    context = lws_create_context(&info);
-    if (!context) {
-        fprintf(stderr, "lws_create_context failed\n");
-        return -1;
-    }
+  context = lws_create_context(&info);
+  if (!context) {
+    fprintf(stderr, "lws_create_context failed\n");
+    return -1;
+  }
 
-    // Connect to the server
-    struct lws_client_connect_info ccinfo = {0};
-    ccinfo.context = context;
-    ccinfo.address = "localhost";
-    ccinfo.port = 6969;
-    ccinfo.path = "/";
-    ccinfo.host = lws_canonical_hostname(context);
-    ccinfo.origin = "localhost";
-    ccinfo.protocol = protocols[0].name;
+  // Connect to the server
+  struct lws_client_connect_info ccinfo = {0};
+  ccinfo.context = context;
+  ccinfo.address = "localhost";
+  ccinfo.port = 6969;
+  ccinfo.path = "/";
+  ccinfo.host = lws_canonical_hostname(context);
+  ccinfo.origin = "localhost";
+  ccinfo.protocol = protocols[0].name;
 
-    lws_client_connect_via_info(&ccinfo);
+  lws_client_connect_via_info(&ccinfo);
 
-    // Event loop
-    while (1) {
-        lws_service(context, 0);
-    }
+  // Event loop
+  while (1) {
+    lws_service(context, 0);
+  }
 
-    lws_context_destroy(context);
-    return 0;
+  lws_context_destroy(context);
+  return 0;
 }
--- websockets/server.c
+++ websockets/server.c
@@ -4,55 +4,58 @@
 #include <libwebsockets.h>
 
 static int callback_echo(struct lws *wsi, 
-                          enum lws_callback_reasons reason, 
-                          void *user, 
-                          void *in, 
-                          size_t len) {
-    switch (reason) {
-        case LWS_CALLBACK_RECEIVE:
-            // Echo the received message back to the client
-            lws_write(wsi, in, len, LWS_WRITE_TEXT);
-            break;
-        default:
-            break;
-    }
-    return 0;
+			 enum lws_callback_reasons reason, 
+			 void *user, 
+			 void *in, 
+			 size_t len) {
+  printf("Entered Callback function\n");
+  switch (reason) {
+  case LWS_CALLBACK_RECEIVE:
+    // Echo the received message back to the client
+    printf("Rx'ed: %s\n", (char*)in);
+    lws_write(wsi, in, len, LWS_WRITE_TEXT);
+    break;
+  default:
+    printf("DEFAULT\n");
+    break;
+  }
+  return 0;
 }
 
 static struct lws_protocols protocols[] = {
-    {
-        "echo-protocol",
-        callback_echo,
-        0,
-        128,
-    },
-    { NULL, NULL, 0, 0 } // terminator
+  {
+    "client-protocol",
+    callback_echo,
+    0,
+    128,
+  },
+  { NULL, NULL, 0, 0 } // terminator
 };
 
 int main() {
-    struct lws_context_creation_info info;
-    struct lws_context *context;
+  struct lws_context_creation_info info;
+  struct lws_context *context;
 
-    memset(&info, 0, sizeof(info));
-    info.vhost_name="127.0.0.1";
-    info.port = 6969; // Port number
-    info.protocols = protocols;
-    info.options = LWS_SERVER_OPTION_ALLOW_NON_SSL_ON_SSL_PORT | LWS_SERVER_OPTION_DISABLE_IPV6;
-    info.gid = -1;
-    info.uid = -1;
+  memset(&info, 0, sizeof(info));
+  info.vhost_name="127.0.0.1";
+  info.port = 6969; // Port number
+  info.protocols = protocols;
+  info.options = LWS_SERVER_OPTION_ALLOW_NON_SSL_ON_SSL_PORT | LWS_SERVER_OPTION_DISABLE_IPV6;
+  info.gid = -1;
+  info.uid = -1;
 
-    lws_set_log_level(LLL_DEBUG | LLL_INFO | LLL_NOTICE | LLL_WARN | LLL_ERR, NULL);
-    context = lws_create_context(&info);
-    if (!context) {
-        fprintf(stderr, "lws_create_context failed\n");
-        return -1;
-    }
+  lws_set_log_level(LLL_DEBUG | LLL_INFO | LLL_NOTICE | LLL_WARN | LLL_ERR, NULL);
+  context = lws_create_context(&info);
+  if (!context) {
+    fprintf(stderr, "lws_create_context failed\n");
+    return -1;
+  }
 
-    // Event loop
-    while (1) {
-        lws_service(context, 0);
-    }
+  // Event loop
+  while (1) {
+    lws_service(context, 0);
+  }
 
-    lws_context_destroy(context);
-    return 0;
+  lws_context_destroy(context);
+  return 0;
 }
