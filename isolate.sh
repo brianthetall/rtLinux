3a16df2dc7657c6d7296024e20197d238c103fd2
Brian Wagner <brianthetall@gmail.com>
2025-09-03 03:18:13 +0100

    Updates to the isolate scripts.

--- isolate.sh
+++ isolate.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+#https://documentation.ubuntu.com/real-time/latest/how-to/isolate-workload-cpusets/
+
 sudo systemctl set-property --runtime custom-workload.slice AllowedCPUs=0
 sudo systemctl set-property --runtime init.scope AllowedCPUs=1-3
 sudo systemctl set-property --runtime system.slice AllowedCPUs=1-3
 sudo systemctl set-property --runtime user.slice AllowedCPUs=1-3
-
-#systemd-run --scope -p Slice=custom-workload.slice /home/ubuntu/my-ap
---
+++ runIsolated.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#https://documentation.ubuntu.com/real-time/latest/how-to/isolate-workload-cpusets/
+
+systemd-run --scope -p Slice=custom-workload.slice $1 $2
