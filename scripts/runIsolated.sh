d70e2f7a1204ea535f18e0fbace915f52147a443
Brian Wagner <brianthetall@gmail.com>
2025-09-13 16:20:56 +0100

    Fixed the mandatory password entry to start gogoGadget.sh

--- scripts/runIsolated.sh
+++ scripts/runIsolated.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #https://documentation.ubuntu.com/real-time/latest/how-to/isolate-workload-cpusets/
 
-systemd-run --scope -p Slice=custom-workload.slice $1 $2 $3 $4
+systemd-run --user --scope -p Slice=custom-workload.slice $1 $2 $3 $4
